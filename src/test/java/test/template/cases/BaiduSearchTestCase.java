1dea8d86faf9064c06dda6f039db0ecfd06ae726
ahan <andy.han@activenetwork.com>
2017-04-21 16:32:49 +0800

    Add template

--- src/test/java/test/template/cases/BaiduSearchTestCase.java
+++ src/test/java/test/template/cases/BaiduSearchTestCase.java
@@ -3,12 +3,15 @@ package test.template.cases;
 import org.testng.annotations.Test;
 
 import test.template.cases.base.BaseTestCase;
+import test.template.utils.SeleniumUtils;
 
 public class BaiduSearchTestCase extends BaseTestCase {
 	
 	@Test
 	public void test() {
 		getDriver().get("http://www.baidu.com");
+		SeleniumUtils.takeScreenShot(getDriver());
+
 	}
 	
 }
