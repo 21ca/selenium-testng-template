9fc5b2fa58a1a229a3254d57db1d6128d920dc35
ahan <andy.han@activenetwork.com>
2017-04-21 14:56:58 +0800

    init template

--- src/test/java/test/template/cases/BaiduSearchTestCase.java
+++ src/test/java/test/template/cases/BaiduSearchTestCase.java
@@ -1,18 +1,11 @@
 package test.template.cases;
 
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import test.template.cases.base.BaseTestCase;
-import test.template.common.Settings;
 
 public class BaiduSearchTestCase extends BaseTestCase {
 	
-	@BeforeTest
-	public void setup() {
-		Settings.setupEnvironment("qa");
-
-	}
 	@Test
 	public void test() {
 		getDriver().get("http://www.baidu.com");
