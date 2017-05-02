4bafde385e6e3930e9a07e6b7132b682cd4b7292
ahan <andy.han@activenetwork.com>
2017-05-02 14:58:19 +0800

    Update TestDateTest.java

--- src/test/java/test/template/cases/demo/TestDataTest.java
+++ src/test/java/test/template/cases/demo/TestDataTest.java
@@ -4,10 +4,16 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import test.template.cases.base.BaseTestCase;
+import test.template.cases.base.NoBrowserBaseTestCase;
 
-public class TestDataTest extends BaseTestCase {
-
+public class TestDataTest extends NoBrowserBaseTestCase {
+	
+	@Test
+	public void testData() {
+		Assert.assertTrue(data("test.name").equals("name1"));
+		Assert.assertTrue(data("test.password").equals("pwd2")); 
+	}
+	
 	@Test(dataProvider = "testdata")
 	public void test(String name, String password) {
 		//From BaseTestCase.properties
