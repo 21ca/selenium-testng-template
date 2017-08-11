d81270fb33c8927d74a4e073fd3fb2423c1923ad
ahan <andy.han@activenetwork.com>
2017-08-11 09:56:37 +0800

    Update JsonDateProviderTest

--- src/test/java/test/template/cases/demo/dataprovider/JsonDataProviderTest.java
+++ src/test/java/test/template/cases/demo/dataprovider/JsonDataProviderTest.java
@@ -1,9 +1,11 @@
 package test.template.cases.demo.dataprovider;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import test.template.cases.base.BaseTestCase;
+import test.template.utils.TestngRetry;
 
 public class JsonDataProviderTest extends BaseTestCase{
 	
@@ -12,8 +14,9 @@ public class JsonDataProviderTest extends BaseTestCase{
 		return dataProvider("user.json", User.class, "json");
 	}
 
-	@Test(dataProvider = "users")
+	@Test(dataProvider = "users", retryAnalyzer = TestngRetry.class)
 	public void testUsers(User user) throws Exception {
+		Assert.assertTrue(user.getPassword().equals("Hello"));
 		log.info("Test with: " + user);
 	}
 }
