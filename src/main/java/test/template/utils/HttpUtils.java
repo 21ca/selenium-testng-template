a59cebae55ff0db7dd87d32288af3bab9020a165
ahan <andy.han@activenetwork.com>
2017-08-31 10:31:14 +0800

    Update HttpUtils to skip SSL cert validation

--- src/main/java/test/template/utils/HttpUtils.java
+++ src/main/java/test/template/utils/HttpUtils.java
@@ -5,13 +5,50 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.apache.commons.io.IOUtils;
 
-
 public class HttpUtils {
+	static {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return new X509Certificate[] {};
+			}
+
+			public void checkClientTrusted(X509Certificate[] chain, String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] chain, String authType) {
+			}
+		} };
+
+		HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+			@Override
+			public boolean verify(String arg0, SSLSession arg1) {
+				return true;
+			}
+		};
+
+		try {
+			SSLContext sc = SSLContext.getInstance("TLS");
+			sc.init(null, trustAllCerts, new SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
+		} catch (Exception e) {
+		}
+	}
+
 	public static final byte[] get(String url, Map<String, String> headers) {
 		try {
 			URLConnection conn = new URL(url).openConnection();
@@ -32,12 +69,12 @@ public class HttpUtils {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public static final byte[] post(String url, String data, Map<String, String> headers) {
 		try {
 			URLConnection conn = new URL(url).openConnection();
 			conn.setDoOutput(true);
-			
+
 			if (headers != null) {
 				for (Map.Entry<String, String> entry : headers.entrySet()) {
 					conn.setRequestProperty(entry.getKey(), entry.getValue());
